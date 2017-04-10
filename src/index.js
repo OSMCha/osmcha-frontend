2273f0fd86c59eb3d9d1d51312f1e58b615a68f6
Kushan Joshi <0o3ko0@gmail.com>
2017-04-10 15:20:51 +0530

    add immutable and hook the redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import {Provider} from 'react-redux';
+import {store} from './store';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
