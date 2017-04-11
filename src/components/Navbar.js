70c4162eb26cbcfc7dca2910737df78ab805ae43
Kushan Joshi <0o3ko0@gmail.com>
2017-04-11 13:33:25 +0530

    add basic classes navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,21 @@ import {Link} from 'react-router-dom';
 export class Navbar extends PureComponent {
   render() {
     return (
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/stats">Stats</Link></li>
-          <li><Link to="/features">Features</Link></li>
-        </ul>
+      <nav className="flex-parent">
+        <div className="align-items--center flex-parent">
+          <div className="flex-child mb-logo mb-logo--gray-dark" />
+          <span
+            className="border-l border--2 txt-fancy color-gray txt-l inline-block pl12 ml12"
+          >
+            OSMCha
+          </span>
+          <ul className="flex-parent flex-parent--row">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/stats">Stats</Link></li>
+            <li><Link to="/features">Features</Link></li>
+          </ul>
+        </div>
       </nav>
     );
   }
