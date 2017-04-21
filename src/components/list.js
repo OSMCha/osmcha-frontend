c582a25b2653202fce4c9d2feb36c8b075891d21
Kushan Joshi <0o3ko0@gmail.com>
2017-04-22 01:59:34 +0530

    decorate sidebar with cdm

--- src/components/list.js
+++ src/components/list.js
@@ -7,7 +7,7 @@ class ListItem extends React.PureComponent {
     return (
       <div
         className={
-          ` ${this.props.active ? 'bg-green-faint' : ''} transition flex-parent p12 flex-parent--center-cross transition bg-gray-light-on-hover h36 border-b border--gray-light border--1`
+          ` ${this.props.active ? 'bg-green-faint' : ''} transition flex-parent p12 flex-parent--center-cross justify--space-between transition bg-gray-light-on-hover h36 border-b border--gray-light border--1`
         }
       >
         <Link
@@ -21,6 +21,23 @@ class ListItem extends React.PureComponent {
         >
           {this.props.id}
         </Link>
+        <div className="flex-child w96 wmin96">
+          <div
+            className="bg-green-faint mr3 color-green inline-block px6 py3 txt-xs txt-bold round-full"
+          >
+            {this.props.properties.get('create')}
+          </div>
+          <div
+            className="bg-orange-faint mr3 color-orange inline-block px6 py3 txt-xs txt-bold round-full"
+          >
+            {this.props.properties.get('modify')}
+          </div>
+          <div
+            className="bg-red-faint mr3 color-red inline-block px6 py3 txt-xs txt-bold round-full"
+          >
+            {this.props.properties.get('delete')}
+          </div>
+        </div>
       </div>
     );
   }
@@ -39,6 +56,7 @@ export class List extends React.PureComponent {
           <ListItem
             key={k}
             id={f.get('id')}
+            properties={f.get('properties')}
             active={f.get('id') === this.props.activeChangesetId}
             fetchChangeset={this.props.fetchChangeset}
             isBold={!this.props.cachedChangesets.has(f.get('id'))}
