c9c43bcb3df389a6108da007af27506abd49d951
Kushan Joshi <0o3ko0@gmail.com>
2017-04-26 17:36:44 +0530

    make it super performant

--- src/components/list.js
+++ src/components/list.js
@@ -101,64 +101,6 @@ class LisstItem extends React.PureComponent {
   }
 }
 
-function CDM(props) {
-  return (
-    <div
-      className={
-        `flex-parent flex-parent--row flex-parent--wrap ${props.className}`
-      }
-    >
-      {props.create > 0 &&
-        <Tooltip
-          position="bottom"
-          theme="osmcha"
-          delay={300}
-          arrow
-          className="flex-child"
-          html={<span className="color-gray">Number of objects Created</span>}
-          animation="perspective"
-        >
-          <div
-            className="bg-green-faint mr3 color-green inline-block px6 py3 txt-xs txt-bold round-full"
-          >
-            {props.create}
-          </div>
-        </Tooltip>}
-      {props.modify > 0 &&
-        <Tooltip
-          position="bottom"
-          theme="osmcha"
-          delay={300}
-          arrow
-          className="flex-child"
-          html={<span className="color-gray">Number of objects Modified</span>}
-          animation="perspective"
-        >
-          <div
-            className="bg-orange-faint mr3 color-orange inline-block px6 py3 txt-xs txt-bold round-full"
-          >
-            {props.modify}
-          </div>{' '}
-        </Tooltip>}
-      {props.delete > 0 &&
-        <Tooltip
-          position="bottom"
-          theme="osmcha"
-          delay={300}
-          arrow
-          className="flex-child"
-          html={<span className="color-gray">Number of objects Deleted</span>}
-          animation="perspective"
-        >
-          <div
-            className="bg-red-faint color-red inline-block px6 py3 txt-xs txt-bold round-full"
-          >
-            {props.delete}
-          </div>{' '}
-        </Tooltip>}
-    </div>
-  );
-}
 export class List extends React.PureComponent {
   props: {
     data: ImmutableList<Map<string, *>>,
@@ -170,54 +112,12 @@ export class List extends React.PureComponent {
     return (
       <ul className="flex-parent flex-parent--column">
         {this.props.data.map((f, k) => (
-          <Link to={`/changesets/${f.get('id')}`}>
+          <Link key={k} to={`/changesets/${f.get('id')}`}>
             <ListItemMulti
               onClick={() => console.log('clicked')}
               active={f.get('id') === this.props.activeChangesetId}
-              title={
-                <span
-                  className="flex-parent flex-parent--row flex-parent--center-cross flex-parent--wrap"
-                >
-                  <span className="txt-m txt-bold mt3 mr6">
-                    {f.get('properties').get('user')}
-                  </span>
-                  <span>
-                    {f.get('properties').get('reasons').map((r, k) => (
-                      <div
-                        key={k}
-                        className="bg-blue-faint mr3 color-blue inline-block px6 py3 txt-xs txt-bold round-full"
-                      >
-                        {r}
-                      </div>
-                    ))}
-                  </span>
-                  <CDM
-                    className="mr3"
-                    create={f.get('properties').get('create')}
-                    modify={f.get('properties').get('modify')}
-                    delete={f.get('properties').get('delete')}
-                  />
-                </span>
-              }
-              minor={
-                <span className="flex-parent flex-parent--row txt-light txt-s">
-                  <span>
-                    Changeset {f.get('id')} created &nbsp;
-                  </span>
-                  <span>
-                    {moment(f.get('properties').get('date')).fromNow()}
-                  </span>
-                </span>
-              }
-              secondary={
-                <span className="flex-parent flex-parent--column">
-                  <p className="flex-child truncate-2-lines my3 txt-em">
-                    {f.get('properties').get('comment')}
-                  </p>
-                </span>
-              }
-              key={k}
-              right={<div />}
+              properties={f.get('properties')}
+              changesetId={f.get('id')}
             />
           </Link>
         ))}
