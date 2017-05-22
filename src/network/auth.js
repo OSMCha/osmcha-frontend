9ad6b17ecf759ab6b911924ab98e22613ebcc537
Kushan Joshi <0o3ko0@gmail.com>
2017-05-22 16:26:16 +0530

    fix login error

--- src/network/auth.js
+++ src/network/auth.js
@@ -1,45 +1,29 @@
 import {osmchaSocialTokenUrl} from '../config/constants';
-import request from 'request';
+import request from 'superagent';
 
 export function postTokensOSMCha(body: Object) {
   let oAuthTokens;
-  let content = {
-    method: 'POST',
-  };
+
   if (body) {
     oAuthTokens = body.oAuthObj;
-    console.log('here', oAuthTokens);
-    // const creatForm = d => {
-    //   var formData = new FormData();
-    //   Object.keys(d).forEach(k => {
-    //     formData.append(k, d[k]);
-    //   });
-    //   return formData;
-    // };
-    // console.log(oAuthTokens);
-    // var data = `oauth_token=${oAuthTokens.oauth_token}&oauth_verifier=${oAuthTokens.oauth_verifier}`;
-
-    // var xhr = new XMLHttpRequest();
-    // xhr.withCredentials = true;
-
-    // xhr.addEventListener('readystatechange', function() {
-    //   if (this.readyState === 4) {
-    //     console.log(this.responseText);
-    //   }
-    // });
-
-    // xhr.open(
-    //   'POST',
-    //   'https://osmcha-django-api-test.tilestream.net/api/v1/login/social/token/openstreetmap'
-    // );
-    // xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-    // xhr.setRequestHeader('cache-control', 'no-cache');
-
-    // xhr.send(data);
-    return;
+    return request
+      .post(
+        'https://osmcha-django-api-test.tilestream.net/api/v1/login/social/token/openstreetmap/',
+      )
+      .type('form')
+      .send({oauth_token: oAuthTokens.oauth_token})
+      .send({oauth_verifier: oAuthTokens.oauth_verifier})
+      .then(r => {
+        console.log('fina;l', r.body);
+        return r.body;
+      });
     // content.body = creatForm(oAuthTokens);
   }
 
-  console.log('sending', content);
-  return fetch(osmchaSocialTokenUrl, content).then(r => r.json());
+  return request
+    .post(
+      'https://osmcha-django-api-test.tilestream.net/api/v1/login/social/token/openstreetmap/',
+    )
+    .type('form')
+    .then(r => r.body);
 }
