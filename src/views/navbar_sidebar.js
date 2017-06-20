efba7b13c989062056d567350f2be8a011a6a443
Kushan Joshi <0o3ko0@gmail.com>
2017-06-20 18:04:59 +0530

    fix

--- src/views/navbar_sidebar.js
+++ src/views/navbar_sidebar.js
@@ -46,12 +46,10 @@ class NavbarSidebar extends React.PureComponent {
     var oAuthToken = this.props.oAuthToken;
     if (!oAuthToken) return;
     let url = `${osmAuthUrl}?oauth_token=${oAuthToken}`;
-    if (isDev) {
-      url = 'osmcha-frontend/local-landing.html';
-    }
-    if (isLocal) {
+    if (isDev || isLocal) {
       url = '/local-landing.html';
     }
+
     if (oAuthToken) {
       const popup = createPopup('oauth_popup', url);
       handlePopupCallback().then(oAuthObj => {
