58a2e998b1a39b1e5b32e9afe4d268780ed09378
Kushan Joshi <0o3ko0@gmail.com>
2017-06-22 18:46:49 +0530

    minor fixes' '

--- src/config/index.js
+++ src/config/index.js
@@ -8,9 +8,9 @@ export const appVersion = process.env.REACT_APP_VERSION;
 
 let url = 'https://osmcha-django-staging.tilestream.net/api/v1';
 
-// if (isProd) {
-url = 'https://osmcha.mapbox.com/api/v1';
-// }
+if (isProd) {
+  url = 'https://osmcha.mapbox.com/api/v1';
+}
 
 window.debug_info = () =>
   `isDev=${isDev.toString()} isStaging=${isStaging.toString()} isProd=${isProd.toString()} isLocal=${isLocal.toString()} stack=${stack ||
