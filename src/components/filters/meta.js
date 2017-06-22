767f128cd24f29315f68470de2eeac886cff0ae2
Kushan Joshi <0o3ko0@gmail.com>
2017-06-22 12:32:29 +0530

    multi filter meta filter stab1

---
+++ src/components/filters/meta.js
@@ -0,0 +1,73 @@
+// @flow
+import React from 'react';
+import { Map, fromJS } from 'immutable';
+import Select, { Creatable, Async } from 'react-select';
+
+export class Meta extends React.PureComponent {
+  options: Array<*>;
+  props: {
+    activeFilters: Map<string, *>,
+    metaOf: Array<string>,
+    options: Array<Object>,
+    onChange: (Map<string, *>) => {}
+  };
+  constructor(props: Object) {
+    super(props);
+    // this.options = props.metaOf.reduce((a, o) => {
+    //   const targetValue = this.props.filters.find(f => f.name === o);
+    //   if (!targetValue || !targetValue.options) {
+    //     throw new Error("cant match the meta filter's options to filter");
+    //   }
+    //   return a.concat(
+    //     targetValue.options.map(v => ({
+    //       label: v.label,
+    //       filter: o,
+    //       value: v.value
+    //     }))
+    //   );
+    // }, []);
+    console.log(this.options);
+  }
+  handleChange = (data: Object) => {
+    let { activeFilters } = this.props;
+    if (!activeFilters) activeFilters = new Map();
+    this.props.metaOf.forEach(f => {
+      activeFilters = activeFilters.delete(f);
+    });
+    if (data && data.value) {
+      activeFilters = activeFilters.merge(fromJS(data.value));
+    }
+    this.props.onChange(activeFilters);
+  };
+  findCurrentValue = () => {
+    const { activeFilters } = this.props;
+    let value;
+    if (activeFilters) {
+      activeFilters.forEach((v, k) => {
+        this.props.options.forEach(option => {
+          if (
+            Object.keys(option.value)[0] === k &&
+            v.getIn([0, 'value']) === option.value[k][0].value
+          ) {
+            value = option;
+          }
+        });
+      });
+    }
+    return value;
+  };
+  render() {
+    const { name, placeholder, activeFilters } = this.props;
+    let value = this.findCurrentValue();
+    return (
+      <Select
+        name={name}
+        value={value} // always takes 1st item array to keep things consistent with multiselect InputTypes
+        options={this.props.options}
+        placeholder={placeholder}
+        className=""
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
--- src/views/filters.js
+++ src/views/filters.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Map, List, Set } from 'immutable';
 import { Link } from 'react-router-dom';
 
-import { Text, Radio, MultiSelect, Wrapper } from '../components/filters';
+import { Text, Radio, MultiSelect, Wrapper, Meta } from '../components/filters';
 
 import { Button } from '../components/button';
 import { BBoxPicker } from '../components/bbox_picker';
@@ -21,7 +21,6 @@ var filtersData = filters.filter(f => {
   return !f.ignore;
 });
 
-const USERS_LIMIT = 200;
 export class _Filters extends React.PureComponent {
   props: {
     filters: Map<string, any>,
@@ -65,6 +64,9 @@ export class _Filters extends React.PureComponent {
       filters: this.state.filters.set(name, values)
     });
   };
+  handleMetaChange = filters => {
+    this.setState({ filters });
+  };
   handleClear = () => {
     this.props.applyFilters(
       new Map(),
@@ -72,32 +74,39 @@ export class _Filters extends React.PureComponent {
     );
   };
   renderFilters = (f: Object, k: number) => {
+    const propsToSend = {
+      name: f.name,
+      type: f.type,
+      display: f.display,
+      value: this.state.filters.get(f.name),
+      placeholder: f.placeholder,
+      options: f.options || [],
+      onChange: this.handleChange,
+      dataURL: f.data_url
+    };
+    const wrapperProps = {
+      name: f.name,
+      handleFocus: this.handleFocus,
+      display: f.display,
+      key: k,
+      description: this.state.active === f.name && f.description
+    };
     if (f.range) {
       return (
-        <Wrapper
-          name={f.name}
-          handleFocus={this.handleFocus}
-          display={f.display}
-          key={k}
-          description={this.state.active === f.name && f.description}
-        >
+        <Wrapper {...wrapperProps}>
           <span className="flex-parent flex-parent--row  ">
             <Text
-              type={f.type}
+              {...propsToSend}
               className="mr3"
-              value={this.state.filters.get(f.name + '__gte')}
               name={f.name + '__gte'}
-              display={f.display}
+              value={this.state.filters.get(f.name + '__gte')}
               placeholder={'from'}
-              onChange={this.handleChange}
             />
             <Text
-              type={f.type}
-              value={this.state.filters.get(f.name + '__lte')}
+              {...propsToSend}
               name={f.name + '__lte'}
-              display={f.display}
+              value={this.state.filters.get(f.name + '__lte')}
               placeholder={'to'}
-              onChange={this.handleChange}
             />
           </span>
         </Wrapper>
@@ -105,73 +114,42 @@ export class _Filters extends React.PureComponent {
     }
     if (f.type === 'text') {
       return (
-        <Wrapper
-          display={f.display}
-          key={k}
-          name={f.name}
-          handleFocus={this.handleFocus}
-          description={this.state.active === f.name && f.description}
-        >
-          <Text
-            type={f.type}
-            value={this.state.filters.get(f.name)}
-            name={f.name}
-            display={f.display}
-            placeholder={f.placeholder}
-            onChange={this.handleChange}
-          />
+        <Wrapper {...wrapperProps}>
+          <Text {...propsToSend} />
         </Wrapper>
       );
     }
     if (f.type === 'radio') {
       return (
-        <Wrapper
-          display={f.display}
-          key={k}
-          name={f.name}
-          handleFocus={this.handleFocus}
-          description={this.state.active === f.name && f.description}
-        >
-          <Radio
-            name={f.name}
-            type={f.type}
-            display={f.display}
-            value={this.state.filters.get(f.name)}
-            placeholder={f.placeholder}
-            options={f.options || []}
-            onChange={this.handleChange}
+        <Wrapper {...wrapperProps}>
+          <Radio {...propsToSend} />
+        </Wrapper>
+      );
+    }
+    if (f.type === 'meta') {
+      return (
+        <Wrapper {...wrapperProps}>
+          <Meta
+            {...propsToSend}
+            onChange={this.handleMetaChange}
+            metaOf={f.metaOf}
+            activeFilters={this.state.filters}
+            filters={filters}
           />
         </Wrapper>
       );
     }
     if (f.type === 'text_comma') {
       return (
-        <Wrapper
-          display={f.display}
-          key={k}
-          name={f.name}
-          handleFocus={this.handleFocus}
-          description={this.state.active === f.name && f.description}
-        >
-          <MultiSelect
-            name={f.name}
-            display={f.display}
-            value={this.state.filters.get(f.name)}
-            placeholder={f.placeholder}
-            options={f.options || []}
-            onChange={this.handleChange}
-            dataURL={f.data_url}
-          />
+        <Wrapper {...wrapperProps}>
+          <MultiSelect {...propsToSend} />
         </Wrapper>
       );
     }
     if (f.type === 'map') {
       return (
         <Wrapper
-          display={f.display}
-          key={k}
-          name={f.name}
-          handleFocus={this.handleFocus}
+          {...wrapperProps}
           description={
             this.state.active === f.name &&
             <BBoxPicker
@@ -181,44 +159,13 @@ export class _Filters extends React.PureComponent {
             />
           }
         >
-          <Text
-            type={f.type}
-            value={this.state.filters.get(f.name)}
-            name={f.name}
-            display={f.display}
-            placeholder={f.placeholder}
-            onChange={this.handleChange}
-          />
+          <Text {...propsToSend} />
         </Wrapper>
       );
     }
   };
   render() {
     const width = window.innerWidth;
-    let usersAutofill;
-    if (this.props.features) {
-      let merged = [];
-      let fromNetwork = Set(
-        this.props.features.map(f => f.getIn(['properties', 'user']))
-      ).toJS();
-
-      if (getItem('usersAutofill')) {
-        let cached = [];
-        try {
-          cached = JSON.parse(getItem('usersAutofill') || '');
-        } catch (e) {
-          console.error(e);
-        }
-        if (Array.isArray(cached)) {
-          merged = Array.from(Set(fromNetwork.concat(cached)));
-          merged.slice(0, USERS_LIMIT);
-        }
-      } else {
-        merged = fromNetwork;
-      }
-      setItem('usersAutofill', JSON.stringify(merged));
-      usersAutofill = merged.map(u => ({ label: u, value: u }));
-    }
     return (
       <div
         className={`flex-parent flex-parent--column changesets-filters bg-white ${width <
@@ -261,16 +208,23 @@ export class _Filters extends React.PureComponent {
             .slice(0, 3)
             .map((f: Object, k) => this.renderFilters(f, k))}
           <h2 className="txt-l mr6 txt-bold mt30  border-b border--gray-light border--1">
-            Validation
+            Flags
           </h2>
           {filtersData
-            .slice(3, 6)
+            .slice(3, 5)
             .map((f: Object, k) => this.renderFilters(f, k))}
           <span className="flex-child flex-child--grow wmin420 wmax435" />
           <h2 className="txt-l mr6 txt-bold mt30  border-b border--gray-light border--1">
-            Changeset Data
+            Review
           </h2>
-          {filtersData.slice(6).map((f: Object, k) => this.renderFilters(f, k))}
+          {filtersData
+            .slice(5, 9)
+            .map((f: Object, k) => this.renderFilters(f, k))}
+          <span className="flex-child flex-child--grow wmin420 wmax435" />
+          <h2 className="txt-l mr6 txt-bold mt30  border-b border--gray-light border--1">
+            Changeset Details
+          </h2>
+          {filtersData.slice(9).map((f: Object, k) => this.renderFilters(f, k))}
           <span className="flex-child flex-child--grow wmin420 wmax435" />
         </div>
       </div>
