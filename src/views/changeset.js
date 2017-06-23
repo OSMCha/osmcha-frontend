2e86421437ed2bf8e85e5307e1e329789a718bc2
Kushan Joshi <0o3ko0@gmail.com>
2017-06-23 14:17:42 +0530

    allow user filter

--- src/views/changeset.js
+++ src/views/changeset.js
@@ -1,12 +1,15 @@
 // @flow
 import React from 'react';
 import { connect } from 'react-redux';
-import { Map } from 'immutable';
+import { List as ImmutableList, Map, fromJS } from 'immutable';
+import Mousetrap from 'mousetrap';
 
 import { Changeset as ChangesetDumb } from '../components/changeset';
 
 import { dispatchEvent } from '../utils/dispatch_event';
 
+import { applyFilters } from '../store/changesets_page_actions';
+import { FILTER_BY_USER } from '../config/bindings';
 import type { RootStateType } from '../store';
 
 class Changeset extends React.PureComponent {
@@ -14,8 +17,34 @@ class Changeset extends React.PureComponent {
     errorChangeset: ?Object, // error of the latest that changeset failed
     location: Object,
     loading: boolean, // loading of the selected changesetId
-    currentChangeset: Map<string, *>,
-    changesetId: number
+    currentChangeset?: Map<string, *>,
+    changesetId: number,
+    applyFilters: (Map<string, *>) => mixed // base 0
+  };
+  componentDidMount() {
+    Mousetrap.bind(FILTER_BY_USER, this.filterChangesetsByUser);
+  }
+  componentWillUnmount() {
+    FILTER_BY_USER.forEach(k => Mousetrap.unbind(k));
+  }
+  filterChangesetsByUser = () => {
+    if (this.props.currentChangeset) {
+      const userName = this.props.currentChangeset.getIn([
+        'properties',
+        'user'
+      ]);
+      this.props.applyFilters(
+        new Map().set(
+          'users',
+          fromJS([
+            {
+              label: userName,
+              value: userName
+            }
+          ])
+        )
+      );
+    }
   };
   showChangeset = () => {
     const {
@@ -43,6 +72,7 @@ class Changeset extends React.PureComponent {
         changesetId={changesetId}
         currentChangeset={currentChangeset}
         errorChangeset={errorChangeset}
+        filterChangesetsByUser={this.filterChangesetsByUser}
       />
     );
   };
@@ -57,16 +87,19 @@ class Changeset extends React.PureComponent {
   }
 }
 
-Changeset = connect((state: RootStateType, props) => ({
-  changeset: state.changeset,
-  location: props.location,
-  changesetId: parseInt(props.match.params.id, 10),
-  currentChangeset: state.changeset.getIn([
-    'changesets',
-    parseInt(props.match.params.id, 10)
-  ]),
-  errorChangeset: state.changeset.get('errorChangeset'),
-  loading: state.changeset.get('loading')
-}))(Changeset);
+Changeset = connect(
+  (state: RootStateType, props) => ({
+    changeset: state.changeset,
+    location: props.location,
+    changesetId: parseInt(props.match.params.id, 10),
+    currentChangeset: state.changeset.getIn([
+      'changesets',
+      parseInt(props.match.params.id, 10)
+    ]),
+    errorChangeset: state.changeset.get('errorChangeset'),
+    loading: state.changeset.get('loading')
+  }),
+  { applyFilters }
+)(Changeset);
 
 export { Changeset };
