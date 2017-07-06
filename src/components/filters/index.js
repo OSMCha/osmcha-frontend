9ca4d1ddb27e31a05aba59efb764a5673db6fd2e
Ajith Ranka <ajith@mapbox.com>
2017-07-06 16:08:49 +0530

    replace native date input with react-datepicker

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -5,5 +5,6 @@ export { Text } from './text';
 export { MultiSelect } from './multi_select';
 export { Wrapper } from './wrapper';
 export { Meta } from './meta';
+export { Date } from './date';
 
 export type InputType = Map<'label' | 'value', string>;
