3e7f7d38000d5d688919288747bb1ba552eaca7f
Wille Marcel <wille@wille.blog.br>
2018-04-25 18:39:03 -0300

    indicate when there is an active filter

--- src/components/button.js
+++ src/components/button.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
-export function Button({ onClick, children, iconName, className }: Object) {
+export function Button({
+  onClick,
+  children,
+  iconName,
+  className,
+  activeBtn
+}: Object) {
   return (
     <button
       onClick={onClick}
       className={`${className ||
-        ''} btn btn--s border border--1 border--darken5 border--darken25-on-hover round bg-darken10 bg-darken5-on-hover color-gray transition ${iconName &&
-      children
-        ? 'pl12 pr6'
-        : ''}`}
+        ''} btn btn--s border border--1 round color-gray ${
+        activeBtn
+          ? 'border--darken10 border--darken50-on-hover bg-darken25 bg-darken10-on-hover'
+          : 'border--darken5 border--darken25-on-hover bg-darken10 bg-darken5-on-hover'
+      } transition ${iconName && children ? 'pl12 pr6' : ''}`}
     >
       {children}
       {iconName && (
         <svg
-          className={`icon w18 h18 inline-block align-middle ${children
-            ? 'pl3 pb3'
-            : 'pb3'}`}
+          className={`icon w18 h18 inline-block align-middle ${
+            children ? 'pl3 pb3' : 'pb3'
+          }`}
         >
           <use xlinkHref={`#icon-${iconName}`} />
         </svg>
