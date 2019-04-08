643a94e47925b05359186ffaa9e3a64243c2485b
Wille Marcel <wille@wille.blog.br>
2019-04-08 13:54:41 +0700

    remove other files

--- src/store/history.js
+++
@@ -1,9 +0,0 @@
-import createHistory from 'history/createBrowserHistory';
-import { isDev } from '../config';
-let historyConfig = {};
-if (isDev) {
-  historyConfig.basename = '/osmcha-frontend';
-}
-
-const history = createHistory(historyConfig);
-export { history };
--- src/utils/number_with_commas.js
+++
@@ -1,5 +0,0 @@
-//@flow
-
-export default function numberWithCommas(num: number): string {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
