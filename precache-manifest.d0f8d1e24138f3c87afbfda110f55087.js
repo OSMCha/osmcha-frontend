1626ad7069834ad1a667da0ca315b88d158050e0
Wille Marcel <wille.yyz@gmail.com>
2020-09-03 09:15:17 -0300

    Updates

---
+++ precache-manifest.d0f8d1e24138f3c87afbfda110f55087.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca19937f112b968349d",
+    "url": "https://staging.osmcha.org/static/js/3.0244872d.chunk.js"
+  },
+  {
+    "revision": "ff3898f05e6679684540",
+    "url": "https://staging.osmcha.org/static/css/main.2e7871b1.chunk.css"
+  },
+  {
+    "revision": "114dd1983680803cdd51",
+    "url": "https://staging.osmcha.org/static/js/runtime~main.81f16882.js"
+  },
+  {
+    "revision": "195ff4f9fd1d7c4ac421ac9623b9c61f",
+    "url": "https://staging.osmcha.org/static/media/work_flow.195ff4f9.png"
+  },
+  {
+    "revision": "22570e40aec71985435f",
+    "url": "https://staging.osmcha.org/static/js/2.e1d3a183.chunk.js"
+  },
+  {
+    "revision": "ff3898f05e6679684540",
+    "url": "https://staging.osmcha.org/static/js/main.384b3e48.chunk.js"
+  },
+  {
+    "revision": "13d765c5811fc9db3eba173459a6a762",
+    "url": "https://staging.osmcha.org/static/media/thumbs-up.13d765c5.svg"
+  },
+  {
+    "revision": "7e04a82a7e23f2adb03169d23beaa602",
+    "url": "https://staging.osmcha.org/static/media/thumbs-down.7e04a82a.svg"
+  },
+  {
+    "revision": "ed2bb0d2fecd447f6a74dd7581448589",
+    "url": "https://staging.osmcha.org/static/media/banner.ed2bb0d2.png"
+  },
+  {
+    "revision": "22570e40aec71985435f",
+    "url": "https://staging.osmcha.org/static/css/2.f4755645.chunk.css"
+  },
+  {
+    "revision": "a24387a2cb94ee86f0aa0fbc771623ef",
+    "url": "https://staging.osmcha.org/index.html"
+  }
+];
