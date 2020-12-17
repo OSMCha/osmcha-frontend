ba38d96a182e52db55fcd0a46a64fc48a5740c17
Wille Marcel <wille.yyz@gmail.com>
2020-12-17 08:45:05 -0300

    Updates

--- static/js/4.294df539.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonposmcha-frontend"]=this["webpackJsonposmcha-frontend"]||[]).push([[4],{631:function(n,o){},633:function(n,o){}}]);
-//# sourceMappingURL=4.294df539.chunk.js.map
---
+++ static/js/4.31b88a6b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonposmcha-frontend"]=this["webpackJsonposmcha-frontend"]||[]).push([[4],{632:function(n,o){},634:function(n,o){}}]);
+//# sourceMappingURL=4.31b88a6b.chunk.js.map
