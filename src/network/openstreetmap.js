1f42d89cfe3dfe2b27fe93ca31c4e9e46e0fae90
Wille Marcel <wille.yyz@gmail.com>
2021-01-14 10:03:57 -0300

    fetch OSM API with json format

--- src/network/openstreetmap.js
+++ src/network/openstreetmap.js
@@ -3,38 +3,22 @@ import { apiOSM } from '../config/constants';
 import { API_URL } from '../config';
 
 export function getUserDetails(uid: number, token: string): Map<'string', *> {
-  const user = {};
-  const fromOSM = fetch(`${apiOSM}/user/${uid}`)
-    .then(r => r.text())
+  const user = { uid: uid };
+  const fromOSM = fetch(`${apiOSM}/user/${uid}.json`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(r => r.json())
     .then(r => {
-      const parser = new DOMParser();
-      const xml = parser.parseFromString(r, 'text/xml');
-      xml.getElementsByTagName('osm');
-      const userXml =
-        xml.getElementsByTagName('osm')[0] &&
-        xml.getElementsByTagName('osm')[0].getElementsByTagName('user')[0];
-
-      user.count =
-        userXml.getElementsByTagName('changesets')[0] &&
-        userXml.getElementsByTagName('changesets')[0].getAttribute('count');
-
-      user.uid = userXml.getAttribute('id');
-
-      user.accountCreated = userXml.getAttribute('account_created');
-
-      user.description =
-        userXml.getElementsByTagName('description')[0] &&
-        userXml.getElementsByTagName('description')[0].innerHTML;
-
-      user.img =
-        userXml.getElementsByTagName('img')[0] &&
-        userXml.getElementsByTagName('img')[0].getAttribute('href');
-      user.name = userXml.getAttribute('display_name');
-
-      return user;
+      const u = r.user;
+      user.count = u.changesets.count;
+      user.accountCreated = u.account_created;
+      user.description = u.description;
+      user.img = u.img && u.img.href;
+      user.name = u.display_name;
+      return fromJS(user);
     })
-    .catch(e => user)
-    .then(user => fromJS(user));
+    .catch(e => user);
 
   const fromOSMCha = fetch(`${API_URL}/user-stats/${uid}/`, {
     method: 'GET',
