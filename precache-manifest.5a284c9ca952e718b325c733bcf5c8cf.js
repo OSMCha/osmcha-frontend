f4c0f55056e6cbf0c62f70f69cd2b624e7e2bbad
Wille Marcel <wille.yyz@gmail.com>
2021-01-21 08:52:47 -0300

    Updates

---
+++ precache-manifest.5a284c9ca952e718b325c733bcf5c8cf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2b032af5eb6537253a5d1c580fb4ca",
+    "url": "https://osmcha.org/index.html"
+  },
+  {
+    "revision": "21e5b45cc0000b6f64f9",
+    "url": "https://osmcha.org/static/css/2.e5aeb2ab.chunk.css"
+  },
+  {
+    "revision": "4ee0b192e83cd3c58893",
+    "url": "https://osmcha.org/static/css/main.9493034b.chunk.css"
+  },
+  {
+    "revision": "21e5b45cc0000b6f64f9",
+    "url": "https://osmcha.org/static/js/2.0ced2125.chunk.js"
+  },
+  {
+    "revision": "59dc5c12b5002672e7f04fcbf15fc018",
+    "url": "https://osmcha.org/static/js/2.0ced2125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8983efa06d6bd1bdde87",
+    "url": "https://osmcha.org/static/js/3.3316aaab.chunk.js"
+  },
+  {
+    "revision": "9cf5b22d36e977d966353f8d9f0345ec",
+    "url": "https://osmcha.org/static/js/3.3316aaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee0b192e83cd3c58893",
+    "url": "https://osmcha.org/static/js/main.9d63d87b.chunk.js"
+  },
+  {
+    "revision": "1962db4d0ff23ad5c24e",
+    "url": "https://osmcha.org/static/js/runtime-main.a0b39dd3.js"
+  },
+  {
+    "revision": "ed2bb0d2fecd447f6a74dd7581448589",
+    "url": "https://osmcha.org/static/media/banner.ed2bb0d2.png"
+  },
+  {
+    "revision": "7e04a82a7e23f2adb03169d23beaa602",
+    "url": "https://osmcha.org/static/media/thumbs-down.7e04a82a.svg"
+  },
+  {
+    "revision": "13d765c5811fc9db3eba173459a6a762",
+    "url": "https://osmcha.org/static/media/thumbs-up.13d765c5.svg"
+  },
+  {
+    "revision": "195ff4f9fd1d7c4ac421ac9623b9c61f",
+    "url": "https://osmcha.org/static/media/work_flow.195ff4f9.png"
+  }
+]);
