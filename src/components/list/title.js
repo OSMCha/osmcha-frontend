ce46cdc1aa12d81894365640441e276595c6b544
Wille Marcel <wille.yyz@gmail.com>
2021-04-30 08:14:43 -0300

    Add icon on changeset list to indicate if user is in trusted/watch lists

--- src/components/list/title.js
+++ src/components/list/title.js
@@ -1,16 +1,48 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
 import moment from 'moment';
 
-export function Title({ properties, wasOpen, date }: Object) {
+import { useIsUserListed } from '../../hooks/UseIsUserListed';
+
+function TitleComponent({
+  properties,
+  wasOpen,
+  date,
+  trustedlist,
+  watchlisted
+}: Object) {
+  const [isInTrustedlist, isInWatchlist] = useIsUserListed(
+    properties.get('user'),
+    properties.get('uid'),
+    trustedlist,
+    watchlisted
+  );
   return (
     <div>
       <span className="flex-parent flex-parent--row justify--space-between align-items--center">
         <strong className={'txt-m mt3 mr6'}>
           {properties.get('user') || <i>OSM User</i>}
+          {isInTrustedlist && (
+            <svg className="icon inline-block align-middle pl3 w18 h18 color-yellow">
+              <use xlinkHref="#icon-star" />
+            </svg>
+          )}
+          {isInWatchlist && (
+            <svg className="icon inline-block align-middle pl3 w18 h18 color-red">
+              <use xlinkHref="#icon-alert" />
+            </svg>
+          )}
         </strong>
         <span className="txt-s mr3">&nbsp;{moment(date).fromNow()}</span>
       </span>
     </div>
   );
 }
+
+const Title = connect((state: RootStateType, props) => ({
+  trustedlist: state.trustedlist.get('trustedlist'),
+  watchlisted: state.watchlist.get('watchlist')
+}))(TitleComponent);
+
+export { Title };
---
+++ src/hooks/UseIsUserListed.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+export const useIsUserListed = (username, uid, trustedlist, watchlist) => {
+  const [isInTrustedlist, setIsInTrustedlist] = useState(false);
+  const [isInWatchlist, setIsInWatchlist] = useState(false);
+
+  useEffect(() => {
+    try {
+      setIsInTrustedlist(trustedlist.indexOf(username) !== -1);
+    } catch (e) {
+      setIsInTrustedlist(false);
+    }
+    try {
+      setIsInWatchlist(
+        watchlist.map(user => user.get('uid')).indexOf(uid) !== -1
+      );
+    } catch (e) {
+      setIsInWatchlist(false);
+    }
+  }, [username, uid, watchlist, trustedlist]);
+
+  return [isInTrustedlist, isInWatchlist];
+};
