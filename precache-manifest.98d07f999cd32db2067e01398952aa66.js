13e5ea6b2a851b5478dfe0298db0b1b03692a7f4
Wille Marcel <wille.yyz@gmail.com>
2021-07-01 09:58:53 -0300

    Updates

---
+++ precache-manifest.98d07f999cd32db2067e01398952aa66.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9636623f94b1dc6b50479fe4185774c",
+    "url": "https://osmcha.org/index.html"
+  },
+  {
+    "revision": "1aba8a40ced7a637f0a9",
+    "url": "https://osmcha.org/static/css/2.14f17b14.chunk.css"
+  },
+  {
+    "revision": "c80eb5584559bc7e1287",
+    "url": "https://osmcha.org/static/css/main.759ed7d3.chunk.css"
+  },
+  {
+    "revision": "1aba8a40ced7a637f0a9",
+    "url": "https://osmcha.org/static/js/2.ca0bd2b7.chunk.js"
+  },
+  {
+    "revision": "532ca25b7e54476ca4fdd3627c5ce590",
+    "url": "https://osmcha.org/static/js/2.ca0bd2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e5bbcac43ed45d5ba3",
+    "url": "https://osmcha.org/static/js/3.70cc969e.chunk.js"
+  },
+  {
+    "revision": "cc111eb067dd9513fd4e32ee464287ae",
+    "url": "https://osmcha.org/static/js/3.70cc969e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80eb5584559bc7e1287",
+    "url": "https://osmcha.org/static/js/main.18d7e623.chunk.js"
+  },
+  {
+    "revision": "4efd9571b2ab7276f529",
+    "url": "https://osmcha.org/static/js/runtime-main.5ada5a5e.js"
+  },
+  {
+    "revision": "ed2bb0d2fecd447f6a74dd7581448589",
+    "url": "https://osmcha.org/static/media/banner.ed2bb0d2.png"
+  },
+  {
+    "revision": "7e04a82a7e23f2adb03169d23beaa602",
+    "url": "https://osmcha.org/static/media/thumbs-down.7e04a82a.svg"
+  },
+  {
+    "revision": "13d765c5811fc9db3eba173459a6a762",
+    "url": "https://osmcha.org/static/media/thumbs-up.13d765c5.svg"
+  },
+  {
+    "revision": "195ff4f9fd1d7c4ac421ac9623b9c61f",
+    "url": "https://osmcha.org/static/media/work_flow.195ff4f9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://staging.osmcha.org/precache-manifest.56b2e32e026caa596da76156703b0545.js"
+  "https://osmcha.org/precache-manifest.98d07f999cd32db2067e01398952aa66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://staging.osmcha.org/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://osmcha.org/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
