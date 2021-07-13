79b0efd0cb5af3e4d65e1fce5f9b4b6134fb1bd1
@thalesbruno <thalesbrno@gmail.com>
2021-07-13 09:13:43 -0300

    Fix PropertyList component

--- src/components/changeset/property_list.js
+++ src/components/changeset/property_list.js
@@ -2,19 +2,16 @@ import React from 'react';
 import Property from './property';
 
 const PropertyList = ({ limit, properties, imageryMatch, sourceMatch }) => {
-  const propertiesList = [];
   return Object.entries(properties)
-    .map(([property, value]) =>
-      propertiesList.push(
-        <Property
-          key={property}
-          property={property}
-          value={value}
-          imageryMatch={imageryMatch}
-          sourceMatch={sourceMatch}
-        />
-      )
-    )
+    .map(([property, value]) => (
+      <Property
+        key={property}
+        property={property}
+        value={value}
+        imageryMatch={imageryMatch}
+        sourceMatch={sourceMatch}
+      />
+    ))
     .slice(limit, limit + 2);
 };
 
