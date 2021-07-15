09f46bed1874cc73e92b7b2e199c0f331135569e
Wille Marcel <wille.yyz@gmail.com>
2021-07-14 21:40:12 -0300

    Show additional changeset's metadata information (#570)
    * Add metadata properties
    
    * Create PropertyList and Property components
    
    * Remove unused code and refactor objects and methods
    
    * Fix PropertyList component
    
    * Improve ordering and add title attr to the metadata pagination buttons
    
    Co-authored-by: @thalesbruno <thalesbrno@gmail.com>

---
+++ src/components/changeset/property_list.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Property from './property';
+
+const PropertyList = ({ limit, properties, imageryMatch, sourceMatch }) => {
+  return Object.entries(properties)
+    .map(([property, value]) => (
+      <Property
+        key={property}
+        property={property}
+        value={value}
+        imageryMatch={imageryMatch}
+        sourceMatch={sourceMatch}
+      />
+    ))
+    .slice(limit, limit + 2);
+};
+
+export default PropertyList;
