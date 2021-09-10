06c7c0aaa9a21b5c9d0e3a0186b53dc1b8edff7c
Tobias <tobias.jordans@betterplace.org>
2021-09-10 23:50:04 +0200

    Extract link to osm user profile to component

---
+++ src/components/changeset/user_osm_link.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const UserOSMLink = ({ userName, linkClasses, children }) => {
+  if (!userName) return null;
+
+  const url = `https://www.openstreetmap.org/user/${userName}`;
+
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Open in OSM"
+      className={
+        linkClasses ||
+        'mx3 btn btn--s border border--1 border--darken5 border--darken25-on-hover round bg-darken10 bg-darken5-on-hover color-gray transition'
+      }
+    >
+      {children}
+    </a>
+  );
+};
