5f391b8c41402a434e509bd24d4d22d4ec5e81bc
Justin Bronn <justin.bronn@maxar.com>
2021-09-24 11:48:32 -0600

    Allow customization of the API URL endpoint.

--- src/config/index.js
+++ src/config/index.js
@@ -5,15 +5,8 @@ export const isProd = process.env.REACT_APP_STACK === 'PRODUCTION';
 export const isLocal = process.env.NODE_ENV === 'development';
 export const stack = process.env.REACT_APP_STACK;
 export const appVersion = process.env.REACT_APP_VERSION;
-
-let url = 'https://staging.osmcha.org/api/v1';
-
-if (isProd) {
-  url = 'https://osmcha.org/api/v1';
-}
+export const API_URL = process.env.REACT_APP_API_URL || (isProd ? 'https://osmcha.org/api/v1': 'https://staging.osmcha.org/api/v1');
 
 window.debug_info = () =>
   `isDev=${isDev.toString()} isStaging=${isStaging.toString()} isProd=${isProd.toString()} isLocal=${isLocal.toString()} stack=${stack ||
-    'null'} appVersion=${appVersion || 'null'} url=${url}`;
-
-export const API_URL = url;
+    'null'} appVersion=${appVersion || 'null'} url=${API_URL}`;
