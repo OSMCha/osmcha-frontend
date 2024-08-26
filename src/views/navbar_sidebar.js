70cbe9eaab11e900fcf55aa6843edbd231bfd572
Wille Marcel <wille.yyz@gmail.com>
2024-08-26 10:48:09 -0300

    Fix DEV stack authentication url with localhost redirect_uri

--- src/views/navbar_sidebar.js
+++ src/views/navbar_sidebar.js
@@ -34,7 +34,11 @@ class NavbarSidebar extends React.PureComponent {
 
   handleLoginClick = () => {
     getAuthUrl().then(res => {
-      window.location.assign(res.auth_url);
+      const authUrl = new URL(res.auth_url);
+      if (process.env.REACT_APP_STACK === 'DEV') {
+        authUrl.searchParams.set('redirect_uri', 'http://localhost:3000');
+      }
+      window.location.assign(authUrl.href);
     });
   };
 
