497593a71c7963cf4f8d2aebaa20e1f3745cbe0f
Jake Low <hello@jakelow.com>
2024-12-02 16:59:58 -0800

    Fix console warning from RelativeTime component

--- src/components/relative_time.js
+++ src/components/relative_time.js
@@ -3,7 +3,7 @@ import { formatDistanceToNow } from 'date-fns';
 
 export function RelativeTime({ datetime, addSuffix = true }: Object) {
   return (
-    <time datetime={datetime.toISOString()} title={datetime.toString()}>
+    <time dateTime={datetime.toISOString()} title={datetime.toString()}>
       {formatDistanceToNow(datetime, { addSuffix })}
     </time>
   );
